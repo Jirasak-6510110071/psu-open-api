577421409ffba340670c3427ecc16ccabfb91e1b
Jirasak Songsri <Jirasak.S.Guy@gmail.com>
2024-04-11 18:17:48 +0700

    Edit Layout Home Page

--- psu-together/src/pages/Home.tsx
+++ psu-together/src/pages/Home.tsx
@@ -4,11 +4,26 @@ import Checkbox from '../components/Checkbox'
 
 function Home() {
   return (
-    <div>
+    <>
       <Navbar></Navbar>
-      <Checkbox></Checkbox>
-    </div>
-    
+      <div className='container mx-4 my-2'>
+        <div className='grid grid-cols-4'>
+          <div className='col-span-1 flex flex-col'>
+          <select className="select select-bordered w-full max-w-xs">
+            <option disabled selected>เลือกรายวิชา</option>
+            <option>240-218</option>
+            <option>240-228</option>
+          </select>
+            <Checkbox></Checkbox>
+          </div>
+          <div className='col-span-3 flex flex-col'>
+            <div className='grid grid-cols-3'>
+              
+            </div>
+        </div>
+        </div>
+      </div>
+    </>
   )
 }
 
