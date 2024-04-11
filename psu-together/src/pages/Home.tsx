cbd5f6ae242616cae6dee7b53e5ab26739d496ca
Pakawat <6510110336@psu.ac.th>
2024-04-11 19:00:07 +0700

    ลบ Checkbox ไปใส่ใน Home แทน กับตกแต่งเปลี่ยนไปใช้ tailwind

--- psu-together/src/pages/Home.tsx
+++ psu-together/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Checkbox from '../components/Checkbox'
 
 function Home() {
   return (
@@ -9,12 +8,32 @@ function Home() {
       <div className='container mx-4 my-2'>
         <div className='grid grid-cols-4'>
           <div className='col-span-1 flex flex-col'>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>เลือกรายวิชา</option>
-            <option>240-218</option>
-            <option>240-228</option>
+          <select className="my-5 select select-bordered w-full max-w-xs font-bold border-black">
+            <option disabled selected className='font-semibold font-sans'>เลือกรายวิชา</option>
+            <option className='font-semibold font-sans'>240-218</option>
+            <option className='font-semibold font-sans'>240-228</option>
           </select>
-            <Checkbox></Checkbox>
+          <div>
+        <h1 className = "my-4 font-bold">รูปแบบการสอน</h1>
+        <div className="form-control">
+            <label className="cursor-pointer label">
+                <span className="label-text font-sans font-semibold">การสอนแบบ Inclass</span>
+                <input type="checkbox" defaultChecked className="mx-4 checkbox checkbox-xs checkbox-success" />
+            </label>
+        </div>
+        <div className="form-control">
+            <label className="cursor-pointer label">
+                <span className="label-text font-sans font-semibold">การสอนแบบออนไลน์</span>
+                <input type="checkbox" defaultChecked className="mx-4 checkbox checkbox-xs checkbox-success" />
+            </label>
+        </div>
+        <div className="form-control">
+            <label className="cursor-pointer label">
+                <span className="label-text font-sans font-semibold">ทั้งสองรูปแบบ</span>
+                <input type="checkbox" defaultChecked className="mx-4 checkbox checkbox-xs checkbox-success" />
+            </label>
+        </div>
+    </div>
           </div>
           <div className='col-span-3 flex flex-col'>
             <div className='grid grid-cols-3'>
