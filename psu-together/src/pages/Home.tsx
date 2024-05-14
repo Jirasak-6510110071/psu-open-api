6dc99cc28db80ce5390fd31f07cec2e823e34cfb
Jirasak Songsri <Jirasak.S.Guy@gmail.com>
2024-05-14 14:44:02 +0700

    แก้ไขหน้า Home

--- psu-together/src/pages/Home.tsx
+++ psu-together/src/pages/Home.tsx
@@ -1,90 +1,106 @@
-import { useState, useEffect } from 'react';
-import Student from '../models/Student';
-import Repo from "../repositories"
-import Card from '../components/Card';
-
-
+import { useContext } from 'react';
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar';
+import { AuthContext } from '../context/AuthProvider';
+import { DataContext } from '../context/DataContext';
+import Tutor from '../models/TutorModel';
+
 
 function Home() {
-  const [studentData, setStudentData] = useState<Student[]>([]);
-  const [selectedValue, setSelectedValue] = useState<string>('');
-
-  const fetchData = async () => {
-    const res = await Repo.Studentdata.getAll()
-    if (res) {
-      setStudentData(res)
-    }
-  }
-
-  const fetchDataByCampus = async () => {
-    const res = await Repo.Studentdata.getByCampusID(selectedValue)
-    if (res) {
-      setStudentData(res)
-    }
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    if (e.target.selectedIndex === 6) {
-      fetchData()
-    } else{
-      setSelectedValue('0' + e.target.selectedIndex.toString());
-    }
+  const { sidebarToggle, studentData } = useContext(AuthContext)
+  const { dataTutor, removeFromList, addToBooking } = useContext(DataContext);
+  const isAvatar = (url: string | undefined) => {
+    if (url === "useravatar.png") return true;
   };
 
-   useEffect(() => {
-    fetchDataByCampus();
-    if (!selectedValue) {
-      fetchData();
-    }
-  }, [selectedValue]);
-  return (
-    <>
-      <Navbar></Navbar>
+  const isSelf = (std_id: string | undefined) => {
+    if (std_id === studentData?.studentId) return true;
+  };
 
-      <div className="container mx-auto">
-        <div className='grid grid-cols-4 gap-2'>
-          <div className='col-span-1 p-2'>
-            <select className="my-5 select select-bordered w-full max-w-xs font-bold border-black" onChange={handleChange}>
-              <option disabled selected className='font-semibold font-sans'>เลือกวิทยาเขต</option>
-              <option className='font-semibold font-sans'>วิทยาเขตหาดใหญ่</option>
-              <option className='font-semibold font-sans'>วิทยาเขตปัตตานี</option>
-              <option className='font-semibold font-sans'>วิทยาเขตภูเก็ต</option>
-              <option className='font-semibold font-sans'>วิทยาเขตสุราษฎร์ธานี</option>
-              <option className='font-semibold font-sans'>วิทยาเขตตรัง</option>
-              <option className='font-semibold font-sans'>ทุกวิทยาเขต</option>
-            </select>
-            <div>
-              <h1 className="my-4 font-bold">รูปแบบการสอน</h1>
-              <div className="form-control">
-                <label className="cursor-pointer label">
-                  <span className="label-text font-sans font-semibold">การสอนแบบ Inclass</span>
-                  <input type="checkbox" defaultChecked className="mx-4 checkbox checkbox-xs checkbox-success" />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="cursor-pointer label">
-                  <span className="label-text font-sans font-semibold">การสอนแบบออนไลน์</span>
-                  <input type="checkbox" defaultChecked className="mx-4 checkbox checkbox-xs checkbox-success" />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="cursor-pointer label">
-                  <span className="label-text font-sans font-semibold">ทั้งสองรูปแบบ</span>
-                  <input type="checkbox" defaultChecked className="mx-4 checkbox checkbox-xs checkbox-success" />
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className='col-span-3 grid grid-cols-3 gap-2'>
-            {studentData.map((item) =>
-              <Card key={item.studentId} StudentData={item} />
-            )}
-          </div>
+  const bookTutor = (id: number, data: Tutor) => {
+    removeFromList(id);
+    addToBooking(data)
+  };
+
+
+  return (
+    <div className='flex'>
+      <Sidebar />
+      <div className={`${sidebarToggle ? "ml-72" : ""} w-full`}>
+        <Navbar />
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <caption className="text-xl font-bold text-gray-800 my-4 text-center">
+              รายการนัดติว
+            </caption>
+            {/* head */}
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="px-4 py-2">ผู้ติว</th>
+                <th className="px-4 py-2">วันที่เวลา และ สถานที่</th>
+                <th className="px-4 py-2">วิชาที่สอน</th>
+                <th className="px-4 py-2"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {dataTutor.map((item) => (
+                <tr key={item.id} className="hover">
+                  <td className="px-4 py-2">
+                    <div className="flex items-center gap-3">
+                      {isAvatar(item.image) ? (
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img src={item.image} alt="Avatar" />
+                          </div>
+                        </div>
+                      ) : (
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img src={`data:image/png;base64,${item.image}`} alt="Avatar" />
+                          </div>
+                        </div>
+                      )}
+                      <div>
+                        <div className="font-bold">{item.std_name}</div>
+                        <div className="text-sm text-gray-500">{item.std_id}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="px-4 py-2">
+                    {`${item.date} | ${item.time}`}
+                    <br />
+                    <span className="badge badge-ghost badge-sm mr-1">{item.location}</span>
+                    <span className="badge badge-ghost badge-sm">{`${item.duration} ชั่วโมง`}</span>
+                  </td>
+                  <td className="px-4 py-2">
+                    {item.subject}
+                    <br />
+                    {item.teachingMode === "online" ? (
+                      <span className="badge badge-accent badge-sm">{item.teachingMode}</span>
+                    ) : (
+                      <span className="badge badge-primary badge-sm">{item.teachingMode}</span>
+                    )}
+                  </td>
+                  {isSelf(item.std_id) ? (
+                    <td className="px-4 py-2"></td>
+                  ) : (
+                    <td className="px-4 py-2">
+                      <button
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => bookTutor(item.id, item)}
+                      >
+                        จอง
+                      </button>
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    </>
+    </div>
   )
 }
-
 export default Home
