3d0527228a6d463ae10de272fb183a1a9f1dedd9
Jirasak Songsri <Jirasak.S.Guy@gmail.com>
2024-05-14 14:45:23 +0700

    เพิ่มหน้า Tutor และ Component ฟอร์มใส่ข้อมูล

---
+++ psu-together/src/components/EventForm.tsx
@@ -0,0 +1,225 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { AuthContext } from '../context/AuthProvider';
+import { DataContext } from '../context/DataContext';
+
+interface EventData {
+    date: string;
+    time: string;
+    location: string;
+    selectedCourse: string;
+    teachingMode: string;
+    duration: number;
+
+}
+
+interface Props {
+    showForm: boolean;
+    setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const EventForm: React.FC<Props> = ({ showForm, setShowForm }) => {
+    const { dataTutor, addToList, addToBooking } = useContext(DataContext);
+    const { studentData, studentImage, studentSubject } = useContext(AuthContext)
+    const [formData, setFormData] = useState<EventData>({
+        date: '',
+        time: '',
+        location: '',
+        selectedCourse: '',
+        teachingMode: '',
+        duration: 1,
+    });
+    const [errors, setErrors] = useState({
+        date: '',
+        time: '',
+        location: '',
+        selectedCourse: '',
+        teachingMode: '',
+        duration: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleCourseChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setFormData({
+            ...formData,
+            selectedCourse: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const newErrors = {
+            date: formData.date ? '' : 'กรุณาเลือกวันที่',
+            time: formData.time ? '' : 'กรุณาเลือกเวลา',
+            location: formData.location ? '' : 'กรุณากรอกสถานที่',
+            selectedCourse: formData.selectedCourse ? '' : 'กรุณาเลือกรายวิชา',
+            teachingMode: formData.teachingMode ? '' : 'กรุณาเลือกรูปแบบการสอน',
+            duration: formData.duration ? '' : 'กรุณาเลือกจำนวนเวลาที่จะสอน',
+        };
+        setErrors(newErrors);
+        const hasErrors = Object.values(newErrors).some((error) => error !== '');
+        if (!hasErrors) {
+            const data = {
+                id: dataTutor.length,
+                std_id: studentData?.studentId,
+                std_name: `${studentData?.studNameThai} ${studentData?.studSnameThai}`,
+                location: formData.location,
+                time: formData.time,
+                date: formData.date,
+                image: studentImage?.pictureBase64,
+                subject: formData.selectedCourse,
+                teachingMode: formData.teachingMode,
+                duration : formData.duration,
+                status: "waiting"
+            }
+            addToList(data);
+            addToBooking(data)
+            handleButtonClick();
+        }
+    };
+
+    const handleButtonClick = () => {
+        setShowForm(!showForm);
+    };
+    useEffect(() => {
+
+    }, [errors]);
+
+    return (
+        <div className="flex justify-center">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96 absolute z-40"
+            >
+                <div className="mb-2">
+                    <label
+                        className="block text-gray-700 font-bold mb-2"
+                        htmlFor="date"
+                    >
+                        วันที่
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="date"
+                        type="date"
+                        name="date"
+                        value={formData.date}
+                        onChange={handleChange}
+                    />
+                    {errors.date && <p className="text-red-500 text-xs italic">{errors.date}</p>}
+                </div>
+                <div className="mb-2">
+                    <label
+                        className="block text-gray-700 font-bold mb-2"
+                        htmlFor="time"
+                    >
+                        เวลา
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="time"
+                        type="time"
+                        name="time"
+                        value={formData.time}
+                        onChange={handleChange}
+                    />
+                    {errors.date && <p className="text-red-500 text-xs italic">{errors.time}</p>}
+                </div>
+                <div className="mb-2">
+                    <label className="block text-gray-700 font-bold mb-2">จำนวนเวลาที่จะสอน (ชั่วโมง)</label>
+                    <input
+                        type="number"
+                        name="duration"
+                        value={formData.duration}
+                        onChange={handleChange}
+                        min="1"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                    {errors.date && <p className="text-red-500 text-xs italic">{errors.duration}</p>}
+                </div>
+                <div className="mb-2">
+                    <label
+                        className="block text-gray-700 font-bold mb-2"
+                        htmlFor="location"
+                    >
+                        สถานที่
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="location"
+                        type="text"
+                        name="location"
+                        value={formData.location}
+                        onChange={handleChange}
+                    />
+                    {errors.date && <p className="text-red-500 text-xs italic">{errors.location}</p>}
+                </div>
+                <div className="mb-2">
+                    <label className="block text-gray-700 font-bold mb-2">รายวิชา</label>
+                    <select
+                        value={formData.selectedCourse}
+                        onChange={handleCourseChange}
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    >
+                        <option value="">-- เลือกรายวิชา --</option>
+                        {studentSubject?.map((subject) => (
+                            <option key={subject.subjectId} value={`${subject.subjectCode} ${subject.subjectNameThai}`}>
+                                {`${subject.subjectCode} ${subject.subjectNameThai}`}
+                            </option>
+                        ))}
+                    </select>
+                    {errors.date && <p className="text-red-500 text-xs italic">{errors.selectedCourse}</p>}
+                </div>
+                <div className="mb-2">
+                    <label className="block text-gray-700 font-bold mb-2">รูปแบบการสอน</label>
+                    <div>
+                        <label className="inline-flex items-center mr-4">
+                            <input
+                                type="radio"
+                                name="teachingMode"
+                                value="online"
+                                checked={formData.teachingMode === 'online'}
+                                onChange={handleChange}
+                                className="form-radio"
+                            />
+                            <span className="ml-2">online</span>
+                        </label>
+                        <label className="inline-flex items-center">
+                            <input
+                                type="radio"
+                                name="teachingMode"
+                                value="onsite"
+                                checked={formData.teachingMode === 'onsite'}
+                                onChange={handleChange}
+                                className="form-radio"
+                            />
+                            <span className="ml-2">on-site</span>
+                        </label>
+                        {errors.date && <p className="text-red-500 text-xs italic">{errors.teachingMode}</p>}
+                    </div>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                    <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        onClick={handleButtonClick}
+                    >
+                        Close
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default EventForm;
---
+++ psu-together/src/pages/Tutor.tsx
@@ -0,0 +1,185 @@
+import { useContext, useEffect, useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+import { AuthContext } from '../context/AuthProvider';
+import { DataContext } from '../context/DataContext';
+import EventForm from '../components/EventForm';
+import TutorModel from '../models/TutorModel';
+
+
+
+function Tutor() {
+    const { dataTutor, removeFromList, removeFromBooking, booking } = useContext(DataContext);
+    const { sidebarToggle, studentData } = useContext(AuthContext);
+    const [showForm, setShowForm] = useState(false);
+    const [myTutor, setMyTutor] = useState<TutorModel[]>([]);
+    const [myBooking, setMyBooking] = useState<TutorModel[]>([]);
+    const [showMyTutors, setShowMyTutors] = useState(true);
+
+    const handleButtonClick = () => {
+        setShowForm(!showForm);
+    };
+
+    const removeBooking = (id: number) => {
+        removeFromList(id);
+        removeFromBooking(id);
+    };
+
+    useEffect(() => {
+        const filterData = dataTutor.filter(item => item.std_id === studentData?.studentId);
+        const filterBooking = booking.filter(item => item.std_id !== studentData?.studentId);
+        setMyTutor(filterData)
+        setMyBooking(filterBooking)
+    }, [dataTutor]);
+    return (
+        <div className='flex'>
+            <Sidebar />
+            <div className={`${sidebarToggle ? "ml-72" : ""} w-full`}>
+                <Navbar />
+
+                <div>
+                    {showForm && <EventForm showForm={showForm} setShowForm={setShowForm} />}
+                </div>
+                <div className="flex justify-between items-center mb-4">
+                    <div className="flex flex-wrap border-b border-gray-200">
+                        <button
+                            className={`px-6 py-3 font-medium text-gray-600 hover:text-gray-800 focus:outline-none ${showMyTutors
+                                ? 'border-b-2 border-blue-500 text-blue-500'
+                                : ''
+                                }`}
+                            onClick={() => setShowMyTutors(true)}
+                        >
+                            รายการนัดติวของฉัน
+                        </button>
+                        <button
+                            className={`px-6 py-3 font-medium text-gray-600 hover:text-gray-800 focus:outline-none ${!showMyTutors
+                                ? 'border-b-2 border-blue-500 text-blue-500'
+                                : ''
+                                }`}
+                            onClick={() => setShowMyTutors(false)}
+                        >
+                            รายการจองติวของฉัน
+                        </button>
+                    </div>
+                    <button
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                        onClick={handleButtonClick}
+                    >
+                        <span className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 mr-2"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            เปิดติว
+                        </span>
+                    </button>
+                </div>
+                {showMyTutors ? (
+                    <div className="overflow-x-auto">
+                        <table className="table w-full">
+                            <caption className="text-xl font-bold text-gray-800 mb-4 text-center">
+                                รายการนัดติวของฉัน
+                            </caption>
+                            {/* head */}
+                            <thead>
+                                <tr className="bg-gray-200">
+                                    <th className="px-4 py-2">Date</th>
+                                    <th className="px-4 py-2">Subject</th>
+                                    <th className="px-4 py-2">Location</th>
+                                    <th className="px-4 py-2">Duration</th>
+                                    <th className="px-4 py-2">Status</th>
+                                    <th className="px-4 py-2"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {myTutor.map((item) => (
+                                    <tr className="hover" key={item.std_id}>
+                                        <td className="px-4 py-2">{`${item.date} ${item.time}`}</td>
+                                        <td className="px-4 py-2">{item.subject}</td>
+                                        <td className="px-4 py-2">{`${item.location}`}</td>
+                                        <td className="px-4 py-2">{`${item.duration} ชั่วโมง`}</td>
+                                        <td className="px-4 py-2">{item.status}</td>
+                                        <td className="px-4 py-2">
+                                            {item.status === "confirmed" ? (
+                                                <button
+                                                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                                                    onClick={() => removeBooking(item.id)}
+                                                >
+                                                    สำเร็จ
+                                                </button>
+                                            ) : (
+                                                <button
+                                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                                    onClick={() => removeBooking(item.id)}
+                                                >
+                                                    ยกเลิก
+                                                </button>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ) : (
+                    <div className="overflow-x-auto">
+                        <table className="table w-full">
+                            <caption className="text-xl font-bold text-gray-800 mb-4 text-center">
+                                รายการจองติวของฉัน
+                            </caption>
+                            {/* head */}
+                            <thead>
+                                <tr className="bg-gray-200">
+                                    <th className="px-4 py-2">Date</th>
+                                    <th className="px-4 py-2">Subject</th>
+                                    <th className="px-4 py-2">Location</th>
+                                    <th className="px-4 py-2">Duration</th>
+                                    <th className="px-4 py-2">Status</th>
+                                    <th className="px-4 py-2"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {myBooking.map((item) => (
+                                    <tr className="hover" key={item.std_id}>
+                                        <td className="px-4 py-2">{`${item.date} ${item.time}`}</td>
+                                        <td className="px-4 py-2">{item.subject}</td>
+                                        <td className="px-4 py-2">{`${item.location}`}</td>
+                                        <td className="px-4 py-2">{`${item.duration} ชั่วโมง`}</td>
+                                        <td className="px-4 py-2">{item.status}</td>
+                                        <td className="px-4 py-2">
+                                            {item.status === "confirmed" ? (
+                                                <button
+                                                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                                                    onClick={() => removeBooking(item.id)}
+                                                >
+                                                    สำเร็จ
+                                                </button>
+                                            ) : (
+                                                <button
+                                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                                    onClick={() => removeBooking(item.id)}
+                                                >
+                                                    ยกเลิก
+                                                </button>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Tutor
